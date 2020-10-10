4196807533c9d88044498e191b835b4f7e8d9d86
Zabitiny <zackmuraca60@gmail.ocm>
2020-10-09 19:09:29 -0700

    moved nav bar along top and spaced everything out

---
+++ feed.php
@@ -0,0 +1,7 @@
+<?php include("includes/includedFiles.php"); ?>
+
+<h1 class="pageHeadingBig">Your Feed</h1>
+
+<div class="gridViewContainer">
+  
+</div>
--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -1,34 +1,20 @@
 <div id="navBarContainer">
-    <nav class="navBar">
-        <span role="link" tabIndex="0" onclick="openPage('index.php')" class="logo">
-            <img src="assets/images/icons/logo.png" alt="">
-        </span>
+  <nav class="navBar">
+    <div class="navBar-left">
+      <a role="link" tabIndex="0" href="index.php" class="logo">
+        <img src="assets/images/icons/logo.png" alt="home">
+      </a>
 
-        <div class="group">
 
-            <div class="navItem">
-
-                <span role='link' tabIndex='0' onclick="openPage('search.php')" class="navItemLink">Search
-                    <img src="assets/images/icons/search.png" class="icon" alt="search">
-                </span>
-
-            </div>
-        </div>
-
-        <div class="group">
-            <div class="navItem">
-            <span role="link" tabIndex="0" onclick="openPage('browse.php')" class="navItemLink">Browse</span>
-            </div>
-
-            <div class="navItem">
-            <span role="link" tabIndex="0" onclick="openPage('yourMusic.php')" class="navItemLink">Your Music</span>
-            </div>
-
-            <div class="navItem">
-            <span role="link" tabIndex="0" onclick="openPage('settings.php')" class="navItemLink"><?php echo $userLoggedIn->getUsername(); ?> </span>
-            </div>
-
-        </div>
-        
-    </nav>
+      <a role="link" tabIndex="0" href="feed.php" class="navItemLink" id="feed">Feed</a>
+    </div>
+    <div class="navBar-center">
+          <a role='link' tabIndex='0' href="search.php" class="navItemLink" id="search">Search
+            <img src="assets/images/icons/search.png" class="icon" alt="search">
+          </a>
+    </div>
+    <div class="navBar-right">
+      <a role="link" tabIndex="0" href="settings.php" class="navItemLink" id="username"><?php echo $userLoggedIn->getUsername(); ?> </a>
+    </div>
+  </nav>
 </div>
